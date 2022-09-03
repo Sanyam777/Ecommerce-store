aa506bff1663866562a625948b35b8a9dd4de122
Sanyam <sanyammrd56@gmail.com>
2022-09-03 21:09:56 +0530

    feat: we have a working store!

---
+++ public/js/stripe-purchase.js
@@ -0,0 +1,28 @@
+export async function handleFormSubmission(event) {
+    event.preventDefault()
+    console.log(event)
+    const form = new FormData(event.target)
+
+    const data = {
+        sku: form.get('sku'),
+        quantity: Number(form.get('quantity'))
+    }
+
+    const response = await fetch('/.netlify/functions/create-checkout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(res => res.json())
+
+    const stripe = Stripe(response.publishableKey)
+    const { error } = await stripe.redirectToCheckout({
+        sessionId: response.sessionId,
+    });
+
+    if (error) {
+        console.log(error)
+    }
+
+}
